2783f8c06f41deb44d0335f58d375de259b07796
glorious-devil <katarzyna.debowska@lo1.sandomierz.pl>
2019-01-29 13:31:59 +0100

    ok

---
+++ python/figura01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import turtle 
+
+
+def main(args):
+    
+    turtle.setup(800, 600)
+    while 1>0:
+        turtle.forward(200)
+        turtle.left(90)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
