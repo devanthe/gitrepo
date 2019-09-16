96b3b2695b17fc97760688ae5eb1139970100a2d
glorious-devil <katarzyna.debowska@lo1.sandomierz.pl>
2019-09-16 10:35:09 +0200

    pętle i instrukcje warunkowe w c++

---
+++ cpp/petla.cpp
@@ -0,0 +1,22 @@
+/*
+ * petla.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    for(int i=0; i <= 20; i += 2) {
+        cout << i << endl;
+        cout << "Hey;;; ";
+        } 
+        /*for(początkowa wartość zmiennej
+     iteracyjnej;warunek iteracji (do kiedy ma się wykonywać)
+     ;co ma się dziać, np ikrementacja (powiększenie o 1: 1++) 
+     lub dekrementacja (zmniejszenie o 1: 1--)*/
+	return 0;
+}
+
