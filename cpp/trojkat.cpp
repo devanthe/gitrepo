6a96c77ed98a7a29a8dd395063b506d02778f61b
glorious-devil <katarzyna.debowska@lo1.sandomierz.pl>
2019-09-20 09:42:54 +0200

    więcej bootstrapu i odrobiona praca domowa z cpp

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -15,13 +15,19 @@ int main(int argc, char **argv)
 
 {   
     float a, b, c;
-    cout << "Podaj długości boków trójkąta:";
-    cin >> a >> b >> c;
+    cout << "Podaj długości boku a:" << endl;
+    cin >> a;
+    cout << "Podaj długości boku b:" << endl;
+    cin >> b;
+    cout << "Podaj długości boku c:" << endl;
+    cin >> c;
     if(a+b>c && a+c>b && b+c>a) {
-        cout << "Jest to trójkąt";} 
-        else{cout << "haha. nie.";}
-	if(sqrt((a*a) + (b*b))==sqrt(c*c)){
-        cout << "Jest to także trójkąt prostokątny.";}
+        cout << "Jest to trójkąt." << endl;
+        if(sqrt((a*a) + (b*b))==sqrt(c*c)){
+        cout << "Jest to także trójkąt prostokątny." << endl;} else{
+            cout << "Ale to nie jest trójkąt prostokątny!" << endl;}} 
+        else{cout << "haha. nie." << endl;}
+	
 	return 0;
 }
 
