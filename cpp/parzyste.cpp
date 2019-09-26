c32763a2a9316d214c02d178b12724e8183b9b89
glorious-devil <katarzyna.debowska@lo1.sandomierz.pl>
2019-09-26 11:41:40 +0200

    wypiłam dziś dwa energetyki, były dobre, telegraf ma także dobre parówki w cieście, mniam, dzień ogólnie dobry, na obiad barszcz czerwony (z uszkami oczywiście), miłego dnia kochani :))))

---
+++ cpp/parzyste.cpp
@@ -0,0 +1,19 @@
+/*
+ * parzyste.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+	for(i = 0; i<=100; i+=1){
+        if(i%2==0){
+            cout << i << endl;}}
+	return 0;
+}
+
