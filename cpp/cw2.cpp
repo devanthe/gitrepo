0e788f9a4ac23974bfad5c528306f823aff45d1d
glorious-devil <katarzyna.debowska@lo1.sandomierz.pl>
2019-10-07 10:36:12 +0200

    dzień całkiem niezły, zrozumiałam trochę z matmy i wypiłam jednego blacka, czuje się świetnie, ale muszę jeszcze żarcie kupić, będzie jeszcze lepiej :)))

---
+++ cpp/cw2.cpp
@@ -0,0 +1,30 @@
+/*
+ * cw2.cpp
+ * 
+ * Copyright 2019  <>
+ * http://ecg.vot.pl/cpp-funkcje
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void drukuj(int staz, int zarobek, int lata){
+    cout << "Pracujesz: " << staz << "lat i zarabiasz " << zarobek << endl;
+    }
+
+int awans(&staz, &zarobek){
+    staz =+ 1;
+    zarobek
+    }
+
+int main(int argc, char **argv)
+{
+	int staz = 1;
+    int zarobek = 100;
+    int lata = 0;
+    cout << "Podaj przewidywany okres pracy w latach: " << endl;
+    cin >> lata;
+	return 0;
+}
+
