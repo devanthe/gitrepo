333975d4abdc608c9f0bae581f700b3ff91b249e
glorious-devil <katarzyna.debowska@lo1.sandomierz.pl>
2019-12-02 10:34:45 +0100

    nope, 2s4ts, ale teź praca domowa kolejna, dzień może być, ale szkoda że nie mam jak r o z m i e n i ć bo automat nie wydaje r e s z t y z banknotów

---
+++ cpp/szukaj.cpp
@@ -0,0 +1,37 @@
+/*
+ * szukaj.cpp
+ * 
+ * w domu rere ale przeszukuje tablicę od końca 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int szukaj_it(int liczby[], int ile, int n){
+    int indeks = -1;
+    for(int i = 0; i < ile; i++) if(liczby[i] == n) return i;
+    return indeks;
+    }
+
+int rere(int liczby[], int lewy, int prawy, int n){
+    if(lewy > prawy){cout << "elementu nie znaleziono" << endl; return -1;} //nie znaleziono elementu e 
+    else {if(liczby[lewy] == n){cout << "element jest" << endl; return lewy;} else {rere(liczby, lewy+1, prawy, n);}}}
+    
+int erer(int liczby[], int lewy, int prawy, int n){ //od końca tutaj
+    if(lewy > prawy){cout << "elementu nie znaleziono" << endl; return -1;} //nie znaleziono elementu e 
+    else {if(liczby[lewy] == n){cout << "element jest" << endl; return lewy;} else {rere(liczby, lewy+1, prawy, n);}}}
+
+int main(int argc, char **argv)
+{
+	int liczby[] = {3, 8, 3, 9, 10, 45, 37};
+    int n;
+    cout << "podaj szukany element" << endl; cin >> n;
+    //int indeks = szukaj_it(liczby, 7, n);
+    int indeks = rere(liczby, 0, 6, n);
+    if(indeks > -1){
+        cout << liczby[indeks] << endl;
+        }
+	return 0;
+}
+
